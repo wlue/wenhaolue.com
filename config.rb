c4a2dddc793d0d46373b65ee787afa7ac1dcb4a6
Wen-Hao Lue <me@wenhaolue.com>
2024-03-23 03:24:59 -0700

    fix ruby version

--- config.rb
+++ config.rb
@@ -1,6 +1,21 @@
 # Require any additional compass plugins here.
 require "susy"
 
+# Fix Compass issues in Ruby >=3.2
+# Monkeypatch the File Class with the exists? method
+unless Dir.respond_to?(:exists?)
+  class << Dir
+    alias_method :exists?, :exist?
+  end
+end
+
+# Monkeypatch the Dir Class with the exists? method
+unless File.respond_to?(:exists?)
+  class << File
+    alias_method :exists?, :exist?
+  end
+end
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/css"
